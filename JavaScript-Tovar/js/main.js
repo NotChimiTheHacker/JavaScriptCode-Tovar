4d7e2988bdafab19665faa47054b9088ccdf8cdb
Diego Tovar <128613816+NotChimiTheHacker@users.noreply.github.com>
2023-05-23 09:07:20 -0600

    avance
    pomp y let son funciones para que en la inspección te aparezca un mensaje pomp mensaje letras y let números.

--- JavaScript-Tovar/js/main.js
+++ JavaScript-Tovar/js/main.js
@@ -1,2 +1,25 @@
 console.log("Hello World!")
-alert("Bienvenido a javascript")
+//alert("Bienvenido a javascript")
+
+/*
+Comentario Multilinea
+
+
+let nombre = prompt( "Cual es tu nombre?");
+
+console.log(parseInt(nombre))
+
+console.log(typeof(nombre))
+*/
+let numeroUno = prompt ("Ingresa un numero");
+let numeroDos = prompt("Ingresa un numero");
+
+let resultadoSuma = parseInt(numeroUno) + parseInt (numeroDos);
+let resultadoResta = parseInt(numeroUno) - parseInt (numeroDos);
+let resultadoMulti = parseInt(numeroUno) * parseInt (numeroDos);
+let resultadoDivi = parseInt(numeroUno) / parseInt (numeroDos);
+
+console.log( `El resultado de la suma es: ${ resultadoSuma}`);
+console.log( `El resultado de la resta es: ${ resultadoResta}`);
+console.log( `El resultado de la multiplicacion es: ${ resultadoMulti}`);
+console.log( `El resultado de la division es: ${ resultadoDivi}`);
